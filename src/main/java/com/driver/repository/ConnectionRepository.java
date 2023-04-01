af44f8f093c140dfaa4d6244a8cb77069add3ba0
NirajPatil <nirajpatil@22400.com>
2023-04-01 20:08:26 +0530

    frist commit

--- src/main/java/com/driver/repository/ConnectionRepository.java
+++ src/main/java/com/driver/repository/ConnectionRepository.java
@@ -1,8 +1,2 @@
-package com.driver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConnectionRepository extends JpaRepository<Connection, Integer> {
+package com.driver.repository;public class ConnectionRepository {
 }
